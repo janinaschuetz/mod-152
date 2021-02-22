5735ee73aa0be39fb7968e3b34e5b6072840d3e7
Janina Schütz <janina.schuetz97@gmail.com>
2021-02-22 20:48:27 +0100

    finished first endpoint and prepared heroku

--- server.js
+++ server.js
@@ -1,21 +1,24 @@
 var express = require("express");
-//var bodyParser = require("body-parser");
+var sass = require('node-sass')
+var fs = require("fs");
 const app = express();
 
-/*
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json);
-app.use(bodyParser.raw);
-*/
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static('input'));
 
 app.post('/api/css/scss', function (req, res) {
-    res.send("Test");
-    //res.sendFile("");
+    fs.writeFileSync('input/input.scss', req.body.data.scss, () => {});
+    sass.render({
+        file: "input/input.scss"
+    }, function (err, result) {
+        res.send(result.css.toString());
+    });
 });
 
 app.post('/api/css/less', function (req, res) {
     res.send("POST");
-    //res.sendFile("");
 });
 
 app.listen(3000);
