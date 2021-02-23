e11e7d7944e30ff50a4530145d694bfba8f915d5
Janina Schütz <janina.schuetz97@gmail.com>
2021-02-23 18:27:11 +0100

    added less

--- server.js
+++ server.js
@@ -1,20 +1,27 @@
 var express = require("express");
 var sass = require('node-sass');
+var less = require('less');
 var fs = require("fs");
 const app = express();
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
 app.use(express.static('input'));
 
 app.post('/api/css/scss', function (req, res) {
-    fs.writeFileSync('input/input.scss', req.body.data.scss, () => {});
+    fs.writeFileSync('input/input.scss', req.body.data.scss, () => {
+    });
     sass.render({
         file: "input/input.scss"
     }, function (err, result) {
-        if (!err){
-            res.send(result.css.toString());
+        if (!err) {
+            res.send(
+                "{\n" +
+                "  \"data\": {\n" +
+                "    \"scss\": " + "\"" + result.css.toString() + "\"" +
+                "  }\n" +
+                "}");
         } else {
             res.status(400).send(err);
         }
@@ -22,6 +29,11 @@ app.post('/api/css/scss', function (req, res) {
 });
 
 app.post('/api/css/less', function (req, res) {
+    fs.writeFileSync('input/input.less', req.body.data.less, () => {});
+
+
+
+
     res.send("POST");
 });
 
