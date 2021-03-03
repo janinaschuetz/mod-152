25f8a8c36b76763aea753fcd1a90c93c8f39fea7
Janina Schütz <janina.schuetz97@gmail.com>
2021-03-03 21:23:10 +0100

    added third endpoint

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var sharp = require("sharp");
+var multer = require("multer");
 var express = require("express");
 var sass = require('node-sass');
 var less = require('less');
-var fs = require("fs");
 var app = express();
 var port = 3000;
+var files = new Array(5);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 /**
@@ -39,6 +43,83 @@ app.post('/api/css/less', function (req, res) {
         }
         else {
             res.status(400).send(err);
+        }
+    });
+});
+/**
+ * multer configurations
+ * what happens to a file if uploaded
+ */
+var store = multer.diskStorage({
+    // where does the file get stored
+    destination: function (req, file, callback) {
+        callback(null, __dirname + '/uploads');
+    },
+    // how does the file get named
+    filename: function (req, file, callback) {
+        if (file.mimetype.indexOf("image") > -1) {
+            callback(null, Date.now() + '_' + file.originalname);
+        }
+        else {
+            callback({
+                error: 'Not a image file'
+            }, null);
+        }
+    }
+});
+var upload = multer({ storage: store });
+/**
+ * Static directory for files
+ */
+app.use('/files', express.static('files'));
+/**
+ * Third endpoint to generate images in various sizes from one source-image
+ */
+app.post('/api/file', upload.single('file'), function (req, res, next) {
+    for (var i = 0; i < 5; i++) {
+        var filetype = void 0;
+        var width = void 0;
+        switch (i) {
+            case 0:
+                filetype = 'small_';
+                width = 720;
+                break;
+            case 1:
+                filetype = 'medium_';
+                width = 1280;
+                break;
+            case 2:
+                filetype = 'large_';
+                width = 1920;
+                break;
+            case 3:
+                filetype = 'thumbnail_';
+                width = 360;
+                break;
+            case 4:
+                filetype = 'original_';
+                width = null;
+                break;
+        }
+        var fileDestination = __dirname + '/files/' + filetype + req.file.filename;
+        files[i] = filetype + req.file.filename;
+        sharp(__dirname + '/uploads/' + req.file.filename)
+            .resize(width, null, {
+            fit: "contain"
+        })
+            .toFile(fileDestination);
+    }
+    next();
+}, function (req, res) {
+    res.json({
+        data: {
+            images: {
+                small: 'https://https://m152-bis19p-janina-schuetz.herokuapp.com/files/' + files[0],
+                medium: 'https://https://m152-bis19p-janina-schuetz.herokuapp.com/files/' + files[1],
+                large: 'https://https://m152-bis19p-janina-schuetz.herokuapp.com/files/' + files[2],
+                thumbnail: 'https://https://m152-bis19p-janina-schuetz.herokuapp.com/files/' + files[3],
+                original: 'https://https://m152-bis19p-janina-schuetz.herokuapp.com/files/' + files[4]
+            }
         }
     });
 });
--- server.ts
+++ server.ts
@@ -1,9 +1,12 @@
+import * as sharp from "sharp";
+import * as multer from "multer";
+
 let express = require("express");
 let sass = require('node-sass');
 let less = require('less');
-let fs = require("fs");
 const app = express();
 const port = 3000;
+let files: string[] = new Array(5);
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
@@ -11,7 +14,7 @@ app.use(express.json());
 /**
  * First endpoint to transpile scss into css
  */
-app.post('/api/css/scss', function (req, res) {
+app.post('/api/css/scss', (req, res) => {
     sass.render({
         data: req.body.data.scss
     }, function (err, result) {
@@ -30,7 +33,7 @@ app.post('/api/css/scss', function (req, res) {
 /**
  * Second endpoint to transpile less into css
  */
-app.post('/api/css/less', function (req, res) {
+app.post('/api/css/less', (req, res) => {
     less.render(req.body.data.less, function (err, result) {
         if (!err) {
             res.json({
@@ -40,6 +43,95 @@ app.post('/api/css/less', function (req, res) {
             });
         } else {
             res.status(400).send(err);
+        }
+    });
+});
+
+/**
+ * multer configurations
+ * what happens to a file if uploaded
+ */
+let store = multer.diskStorage({
+    // where does the file get stored
+    destination: (req, file, callback) => {
+        callback(null, __dirname + '/uploads');
+    },
+    // how does the file get named
+    filename: (req, file, callback) => {
+        if (file.mimetype.indexOf("image") > -1) {
+            callback(null, Date.now() + '_' + file.originalname);
+        } else {
+            callback({
+                error: 'Not a image file'
+            }, null);
+        }
+    }
+});
+
+let upload = multer({storage: store});
+
+/**
+ * Static directory for files
+ */
+app.use('/files', express.static('files'));
+
+/**
+ * Third endpoint to generate images in various sizes from one source-image
+ */
+app.post('/api/file', upload.single('file'), (req,res, next) => {
+    for (let i = 0; i < 5; i++) {
+        let filetype: string;
+        let width: number;
+
+        switch (i) {
+            case 0:
+                filetype = 'small_';
+                width = 720;
+                break;
+
+            case 1:
+                filetype = 'medium_';
+                width = 1280;
+                break;
+
+            case 2:
+                filetype = 'large_';
+                width = 1920;
+                break;
+
+            case 3:
+                filetype = 'thumbnail_';
+                width = 360;
+                break;
+
+            case 4:
+                filetype = 'original_';
+                width = null;
+                break;
+        }
+
+        let fileDestination = __dirname + '/files/' + filetype + req.file.filename;
+
+        files[i] = filetype + req.file.filename;
+
+        sharp(__dirname + '/uploads/' + req.file.filename)
+            .resize(width, null, {
+                fit: "contain"
+            })
+            .toFile(fileDestination);
+    }
+    next();
+
+}, (req, res) => {
+    res.json({
+        data: {
+            images: {
+                small: 'https://https://m152-bis19p-janina-schuetz.herokuapp.com/files/' + files[0],
+                medium: 'https://https://m152-bis19p-janina-schuetz.herokuapp.com/files/' + files [1],
+                large: 'https://https://m152-bis19p-janina-schuetz.herokuapp.com/files/' + files[2],
+                thumbnail: 'https://https://m152-bis19p-janina-schuetz.herokuapp.com/files/' + files[3],
+                original: 'https://https://m152-bis19p-janina-schuetz.herokuapp.com/files/' + files[4]
+            }
         }
     });
 });
